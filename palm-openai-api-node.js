97f9cdaeb86e8906ec46aeecb4a02f8b9d5a3c55
superuben <superuben@live.nl>
2023-12-08 21:14:39 +0100

    Update palm-openai-api-node.js

--- palm-openai-api-node.js
+++ palm-openai-api-node.js
@@ -3,7 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
-const fetch = require('node-fetch');
+const fetchModule = import('node-fetch');
+const fetch = fetchModule.default || fetchModule;
 
 // Create an Express application
 const app = express();
